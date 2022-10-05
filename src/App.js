27eb5cc3727786e57ee1bf202ffad5ad3088eff8
Stacy Yu <stacyyuu@gmail.com>
2022-10-04 20:57:20 -0700

    added axis and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <h1> City Explorer</h1>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      location: {}
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchQuery: event.target.value })
+  }
+
+
+  getLocation = async () => {
+    // base url
+    // ? is called a query, everything that comes after will be everything we are asking data about 
+    // key: YOUR_ACCESS_TOKEN
+    // q: SEARCH_STRING
+    // format: 'json'
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+    console.log('URL: ', url);
+    const response = await axios.get(url);
+    console.log('Response Object: ', response);
+    console.log('Response Object: ', response.data[0]);
+    this.setState({ location: response.data[0] });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>City Explorer</h1>
+
+        <input
+          type="text"
+          onChange={this.handleChange}
+          placeholder="search for a city"
+        />
+
+        <button onClick={this.getLocation}>Explore!</button>
+        {this.state.location.display_name &&
+          <h2>The city we searched for is {this.state.location.display_name}</h2>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
