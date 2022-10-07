be6011aa6a9a7f9dd3709cd0a3fcfff6350d67f8
Stacy Yu <stacyyuu@gmail.com>
2022-10-06 23:29:37 -0700

    lab6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import './App.css';
 import City from './City';
-
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      location: {}
+    }
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({ searchQuery: event.target.value })
+  }
+
+  getLocation = async (event) => {
+    // base url
+    // ? is called a query, everything that comes after will be everything we are asking data about 
+    // key: YOUR_ACCESS_TOKEN
+    // q: SEARCH_STRING
+    // format: 'json'
+    event.preventDefault();
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+    console.log('URL: ', url);
+    const response = await axios.get(url);
+    console.log('response object: ', response);
+    console.log('response.data[0]: ', response.data[0]);
+    this.setState({ location: response.data[0] });
+  }
 
   render() {
     return (
       <div className="App">
         <h1>City Explorer</h1>
-        <City />
+        <City
+          location={this.state.location}
+          handleChange={this.handleChange}
+          getLocation={this.getLocation}
+        />
       </div>
     );
   }
--- src/City.js
+++ src/City.js
@@ -2,50 +2,28 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+
 
 class City extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: '',
-      location: {}
-    }
-  }
-
-  handleChange = (event) => {
-    this.setState({ searchQuery: event.target.value })
-  }
-
-  getLocation = async () => {
-    // base url
-    // ? is called a query, everything that comes after will be everything we are asking data about 
-    // key: YOUR_ACCESS_TOKEN
-    // q: SEARCH_STRING
-    // format: 'json'
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
-    console.log('URL: ', url);
-    const response = await axios.get(url);
-    console.log('response object: ', response);
-    console.log('response.data[0]: ', response.data[0]);
-    this.setState({ location: response.data[0] });
-  }
 
   render() {
     return (
       <>
         <Container>
-          <Form>
-            <Form.Control type="text" onChange={this.handleChange} placeholder="Search for a city" />
-            <Button variant="primary" type="submit" onClick={this.getLocation}>
+          <Form onSubmit={this.props.getLocation}>
+            <Form.Control type="text" onChange={this.props.handleChange} placeholder="Search for a city" />
+            <Button variant="primary" type="submit">
               Explore!
             </Button>
           </Form>
         </Container>
 
-        {this.state.location.display_name &&
-          <h2>The city we searched for is {this.state.location.display_name}</h2>
+        {this.props.location.display_name &&
+          <h2>The city we searched for is {this.props.location.display_name}</h2>
         }
+        {<h2>The latitude is: {this.props.location.lat} </h2>}
+        {<h2>The longitude is: {this.props.location.lon}</h2>}
+
       </>
     )
   }
